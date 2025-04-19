e8643a2a6d7127a363fc8077e96f3976f01ddc5d
AnaJiRo <anajr991@gmail.com>
2025-04-19 21:22:06 +0200

    feat. Add update y delete

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,9 +1,12 @@
 # users/urls.py
 
 from django.urls import path
-from .views import RegisterUserView, ListUserView
+from .views import RegisterUserView, ListUserView, RetrieveUserView, UpdateUserView, DeleteUserView
 
 urlpatterns = [
     path('register/', RegisterUserView.as_view(), name='register'),
-    path('', ListUserView.as_view() , name= 'users' )
+    path('', ListUserView.as_view() , name= 'users' ),
+    path('<int:id>/', RetrieveUserView.as_view(), name='get_user'),  # <-- detalle por ID
+    path('<int:id>/update/', UpdateUserView.as_view(), name='update_user'),  
+    path('<int:id>/delete/', DeleteUserView.as_view(), name='delete_user'), # <-- delete por ID
 ]
--- backend/users/views.py
+++ backend/users/views.py
@@ -3,13 +3,36 @@
 from rest_framework import generics
 from django.contrib.auth import get_user_model
 from .serializers import UserSerializer
+from rest_framework.permissions import IsAuthenticated
+    
 
 User = get_user_model()
 
 class RegisterUserView(generics.CreateAPIView):
+    """ Crear un usuario """
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
 class ListUserView(generics.ListAPIView):
+    """ Listar todos los usuarios """
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+class RetrieveUserView(generics.RetrieveAPIView):
+    """ Mostrar ususario """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    lookup_field = 'id'
+
+class UpdateUserView(generics.UpdateAPIView):
+    """ Editar usuario: put todo el usuario, path un campo en concreto"""
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    lookup_field = 'id'
+    permission_classes = [IsAuthenticated] # --> Requiere estar logueado.
+
+class DeleteUserView(generics.DestroyAPIView):
+    """ Eliminar usuario """
+    queryset = User.objects.all()
+    lookup_field = 'id'
+    
