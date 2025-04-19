83521ba2832941676ed5feb2b20f42aac165af96
AnaJiRo <anajr991@gmail.com>
2025-04-19 19:59:12 +0200

    feat: add custorm user

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    ROL_CHOICES = (
+        ('admin', 'Administrador'),
+        ('voluntary', 'Voluntario'),
+    )
+
+    name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15, blank=True, null=True)
+    rol = models.CharField(max_length=10, choices=ROL_CHOICES)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'name', 'rol']
+
+    def __str__(self):
+        return self.email
