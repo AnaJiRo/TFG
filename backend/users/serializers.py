790136047676e2c9ca6090711af772b8937cf68c
AnaJiRo <anajr991@gmail.com>
2025-04-19 20:25:49 +0200

    feat: add serializers

---
+++ backend/users/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'name', 'email', 'phone', 'rol', 'password']
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = User(**validated_data)
+        user.set_password(password)  # muy importante: encripta la contraseña
+        user.save()
+        return user
+
+    def update(self, instance, validated_data):
+        if 'password' in validated_data:
+            password = validated_data.pop('password')
+            instance.set_password(password)
+        return super().update(instance, validated_data)
