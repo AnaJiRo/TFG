aa29e7cd6d26a13e826c4b9e5fb7c3dea0b47bef
AnaJiRo <anajr991@gmail.com>
2025-04-20 17:57:56 +0200

    feat: Add login

--- backend/gestor_backend/settings.py
+++ backend/gestor_backend/settings.py
@@ -10,8 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
-from pathlib import Path
 import os
+from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -117,6 +118,22 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
+
+
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'AUTH_HEADER_TYPES': ('Bearer',),
+}
 
 
 # Internationalization
--- backend/users/urls.py
+++ backend/users/urls.py
@@ -2,6 +2,7 @@
 
 from django.urls import path
 from .views import RegisterUserView, ListUserView, RetrieveUserView, UpdateUserView, DeleteUserView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('register/', RegisterUserView.as_view(), name='register'),
@@ -9,4 +10,6 @@ urlpatterns = [
     path('<int:id>/', RetrieveUserView.as_view(), name='get_user'),  # <-- detalle por ID
     path('<int:id>/update/', UpdateUserView.as_view(), name='update_user'),  
     path('<int:id>/delete/', DeleteUserView.as_view(), name='delete_user'), # <-- delete por ID
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
