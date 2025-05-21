6ec6d6a97cd5989268d004f03a00036352f3eb8f
AnaJiRo <anajr991@gmail.com>
2025-05-21 20:06:20 +0200

    feat: add routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,14 @@
+import { Route, Routes } from 'react-router-dom';
 import LandingPage from './pages/LandingPage'; 
 
 function App() {
-  return <LandingPage />;
+  return (
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      {/* <Route path="/login" element={<LoginPage />} /> */}
+    </Routes>
+  );
 }
+
 
 export default App;
