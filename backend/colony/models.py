00b76a0018b8707d6336e4ce70bea9d4d1269d4f
AnaJiRo <anajr991@gmail.com>
2025-05-24 21:22:41 +0200

    feat: zone name unique

--- backend/colony/models.py
+++ backend/colony/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Zone(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
     locality = models.CharField(max_length=100)
 
     def __str__(self):
