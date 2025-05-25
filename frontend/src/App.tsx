07779a2e676b2c2548048b52a6ab91733b35a47b
AnaJiRo <anajr991@gmail.com>
2025-05-25 18:59:35 +0200

    cambio de styles dasdboardcolonias

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import LandingPage from './pages/LandingPage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import CompleteProfilePage from './pages/CompleteProfilePage';
+import DashboardColoniasPage from './pages/DashboardColoniasPage';
 //import Dashboard from './pages/Dashboard'; // crea un componente si no existe aún
 import PrivateRoute from './routes/PrivateRoute';
 
@@ -13,6 +14,7 @@ function App() {
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
       <Route path="/completar-perfil" element={<CompleteProfilePage />} />
+      <Route path="/colonias" element={<DashboardColoniasPage />} />
 
       {/* 🔐 Ruta protegida */}
       <Route element={<PrivateRoute />}>
