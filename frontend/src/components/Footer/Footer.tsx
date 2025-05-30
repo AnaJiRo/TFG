69cdc6f7d140ff0bec101f5fd8d6153c1e5ee147
AnaJiRo <anajr991@gmail.com>
2025-05-30 14:17:28 +0200

    feat:Add footer

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -2,18 +2,20 @@
 export default function Footer() {
   return (
     <footer className="bg-purple-900 text-white text-sm font-nunito py-4 px-6 mt-auto">
-      <div className="flex flex-col md:flex-row items-center justify-between gap-2">
+      <div className="flex flex-col md:flex-row items-center justify-center gap-4 text-center">
         {/* Texto izquierda */}
-        <p className="text-center md:text-left">
+        <p>
           © {new Date().getFullYear()} Gestor de Colonias. Todos los derechos reservados.
         </p>
 
         {/* Créditos / Enlaces opcionales */}
         <div className="flex gap-4 text-center md:text">
-          <a href="https://github.com/AnaJiRo/TFG" target="_blank" rel="noopener noreferrer" 
+          <a href="https://github.com/AnaJiRo/TFG" 
+            target="_blank" 
+            rel="noopener noreferrer" 
             className="flex items-center gap-1 hover:underline">
             <img src="/assets/github.svg" alt="GitHub" className="w-5 h-5" />
-            <span className="hidden sm:inline">GitHub</span>
+            <span>GitHub</span>
           </a>
           <a href="#" className="hover:underline">Contacto</a>
         </div>
