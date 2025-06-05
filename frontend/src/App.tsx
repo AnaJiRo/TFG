7e650ab8e51fbe5c49f9dcbc306b03ef877b616c
AnaJiRo <anajr991@gmail.com>
2025-06-05 20:34:18 +0200

    feat: add autorizate by role

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,19 @@
-import { Route, Routes } from 'react-router-dom';
-import DefaultLayout from './layouts/DefaultLayout';
-import LandingPage from './pages/LandingPage'; 
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
-import CompleteProfilePage from './pages/CompleteProfilePage';
-import DashboardColoniasPage from './pages/DashboardColoniasPage';
-import NuevaColoniaPage from './pages/NuevaColoniaPage';
+import { Route, Routes } from "react-router-dom";
+import DefaultLayout from "./layouts/DefaultLayout";
+import LandingPage from "./pages/LandingPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import CompleteProfilePage from "./pages/CompleteProfilePage";
+import DashboardColoniasPage from "./pages/DashboardColoniasPage";
+import NuevaColoniaPage from "./pages/NuevaColoniaPage";
 //import Dashboard from './pages/Dashboard'; // crea un componente si no existe aún
-import PrivateRoute from './routes/PrivateRoute';
-import DetalleColoniaPage from './pages/DetalleColoniaPage';
-import EditarColoniaPage from './pages/EditarColoniaPage';
-import UserManagementPage from './pages/UserManagementPage';
-import EditUserPage from './pages/EditUserPage';
-import UserProfilePage from './pages/UserProfilePage'
-import EditProfilePage from './pages/EditProfilePage';
+import PrivateRoute from "./routes/PrivateRoute";
+import DetalleColoniaPage from "./pages/DetalleColoniaPage";
+import EditarColoniaPage from "./pages/EditarColoniaPage";
+import UserManagementPage from "./pages/UserManagementPage";
+import EditUserPage from "./pages/EditUserPage";
+import UserProfilePage from "./pages/UserProfilePage";
+import EditProfilePage from "./pages/EditProfilePage";
 
 function App() {
   return (
@@ -24,10 +24,18 @@ function App() {
         <Route path="/colonias/nueva" element={<NuevaColoniaPage />} />
         <Route path="/colonias/:id" element={<DetalleColoniaPage />} />
         <Route path="/colonias/:id/editar" element={<EditarColoniaPage />} />
-        <Route path="/admin/users" element={<UserManagementPage />} />
+
         <Route path="/admin/users/:id/edit" element={<EditUserPage />} />
         <Route path="/profile" element={<UserProfilePage />} />
         <Route path="/profile/:id/edit" element={<EditProfilePage />} />
+        <Route
+          path="/unauthorized"
+          element={
+            <p className="text-center mt-8">
+              No tienes permiso para acceder a esta página.
+            </p>
+          }
+        />
       </Route>
 
       {/* Rutas sin navbar */}
@@ -41,9 +49,11 @@ function App() {
         {/* <Route path="/dashboard" element={<Dashboard />} /> */}
         {/* Aquí puedes meter más rutas privadas si quieres */}
       </Route>
+      <Route element={<PrivateRoute allowedRoles={["admin"]} />}>
+        <Route path="/admin/users" element={<UserManagementPage />} />
+      </Route>
     </Routes>
   );
 }
-
 
 export default App;
--- frontend/src/routes/PrivateRoute.tsx
+++ frontend/src/routes/PrivateRoute.tsx
@@ -1,7 +1,26 @@
-import { Navigate, Outlet } from 'react-router-dom';
+import { Navigate, Outlet } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
 
-export default function PrivateRoute() {
-  const token = localStorage.getItem('access_token');
+interface PrivateRouteProps {
+  allowedRoles?: string[]; // por ejemplo: ['admin'] o ['volunteer']
+}
 
-  return token ? <Outlet /> : <Navigate to="/login" replace />;
+export default function PrivateRoute({ allowedRoles }: PrivateRouteProps) {
+  const token = localStorage.getItem("access_token");
+
+  if (!token) return <Navigate to="/login" replace />;
+
+  try {
+    const decoded: any = jwtDecode(token);
+    const userRole = decoded.role;
+
+    if (allowedRoles && !allowedRoles.includes(userRole)) {
+      return <Navigate to="/unauthorized" replace />;
+    }
+
+    return <Outlet />;
+  } catch (err) {
+    console.error("Error al decodificar el token:", err);
+    return <Navigate to="/login" replace />;
+  }
 }
